c1322009643dff3f7aa640aef2f96d538d0c9df7
Wadson Manzoli <98614603+Wadson126@users.noreply.github.com>
2026-02-19 02:28:00 +0000

    inplace

--- fipe.py
+++ fipe.py
@@ -100,15 +100,16 @@ if vehicleType is not None:
         if modelo_selecionado:
             dados = get_fipe(vehicleType, brandId, modelId, yearId)
             df = pd.DataFrame([dados])
-            df = df.rename(columns = {'price':'Preço',
+            df.rename(columns = {'price':'Preço',
                                       'brand':'Marca', 
                                       'model':'Modelo', 
                                       'modelYear':'Ano',
                                       'fuel': 'Combustível',
                                       'codeFipe': 'Codigo Fipe',
                                       'referenceMonth': 'Mês de Referência'
-                                      })
-            df = df.drop(columns = {'vehicleType', 'fuelAcronym'})
+                                      }, inplace = True)
+            
+            df.drop(columns = {'vehicleType', 'fuelAcronym'}, inplace = True)
             st.dataframe(df, use_container_width=True, hide_index=True)
 
 
